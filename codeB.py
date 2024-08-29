abc156458601b85f8429caaeaac955f8d57b6656
Hungming Yang <homing.yang@gmail.com>
2024-08-29 15:27:40 +0800

    test

--- codeB.py
+++ codeB.py
@@ -1,12 +1,5 @@
 import os
 import re
 # first
-print('hello~~') # 1st main
-<<<<<<< HEAD
-print('hello~~') # 1st 1.2
-print('hello~~') # 1st 1.3
-print('hello~~') # 2.1
-print('hello~~') # 2.2
-=======
-print('hello~~') # 1st 1.2
->>>>>>> parent of c61ca7a (1.2)
+
+print('hello~~') # 2.2 fight
