9b80309ee594aeaac3575677b04f40bd04bfb2a9
Hungming Yang <homing.yang@gmail.com>
2024-08-29 15:48:19 +0800

    fix conflict

--- codeB.py
+++ codeB.py
@@ -3,10 +3,9 @@ import re
 # first
 
 print('hello~~') # 2.2 fight
-<<<<<<< HEAD
+
 print('hello~~') # 3.1 fight
 print('hello~~') # 3.2 fight
 
-=======
+
 print('pc2')
->>>>>>> 80f3aefcb826984f68288c328dea9af505eb955f
